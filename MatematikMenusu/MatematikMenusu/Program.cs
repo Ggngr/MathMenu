a2d3d1e881202eeadd592c19ce624d5539c0a612
Ggngr <gngrbilisim@gmail.com>
2023-03-01 23:41:09 +0300

    MathMenu

---
+++ MatematikMenusu/MatematikMenusu/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatematikMenusu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int islem;
+            Console.WriteLine("Matematik Menüsü :)");
+            Console.WriteLine();
+            Console.WriteLine("1) Aritmetik dört işlem");
+            Console.WriteLine("2) Üs alma");
+            Console.WriteLine("3) Kök alma");
+            Console.WriteLine("4) Karenin çevresi ve alanı");
+            Console.WriteLine("5) Dikdörtgen çevresi ve alanı");
+            Console.WriteLine("6) Eşkenar üçgen çevresi\n");
+
+            Console.WriteLine("*************\n");
+            Console.WriteLine("işlem yapmak istediğiniz alanın numarasını yazın: ");
+            islem = Convert.ToInt32(Console.ReadLine());
+            if (islem == 1)
+            {
+                double a, b, toplam, fark, carpim, bolum;
+                Console.WriteLine("1. sayıyı giriniz");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("2. sayıyı giriniz");
+                b = Convert.ToDouble(Console.ReadLine());
+
+                toplam = a + b;
+                fark = a - b;
+                carpim = a * b;
+                bolum = a / b;
+
+                Console.WriteLine("Toplam = " + toplam);
+                Console.WriteLine("Fark = " + fark);
+                Console.WriteLine("Çarpim = " + carpim);
+                Console.WriteLine("Bölüm = " + bolum);
+                Console.ReadKey();
+
+            }
+            if (islem == 2)
+            {
+                double sayi, us, sonuc;
+                Console.WriteLine("sayıyı giriniz");
+                sayi = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("üssü sayıyı giriniz");
+                us = Convert.ToDouble(Console.ReadLine());
+                sonuc = Math.Pow(sayi, us);
+                Console.WriteLine("Sonuç = " + sonuc);
+                Console.ReadKey();
+
+            }
+            if (islem == 3)
+            {
+                double sayi, kok;
+                Console.WriteLine("Kökü alınacak sayıyı giriniz");
+                sayi = Convert.ToDouble(Console.ReadLine());
+                kok = Math.Sqrt(sayi);
+                Console.WriteLine("Sonuç = " + kok);
+                Console.ReadKey();
+
+            }
+            if (islem == 4)
+            {
+                int kenar, alan, cevre;
+                Console.Write("Kenar değerini giriniz");
+                kenar= Convert.ToInt32(Console.ReadLine());
+                alan = kenar * kenar;
+                cevre = kenar * 4;
+                Console.WriteLine("Alanı = " + alan);
+                Console.WriteLine("Çevresi = " + cevre);
+                Console.ReadKey();
+
+            }
+            if (islem == 5) 
+            {
+                int k1, u1, alan,cevre;
+                Console.WriteLine("Kısa kenar değerini giriniz");
+                k1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Uzun kenar değerini giriniz");
+                u1 = Convert.ToInt32(Console.ReadLine());
+                alan = k1 * u1;
+                cevre = k1 + u1 +k1 +u1;
+                Console.WriteLine("Çevresi = " + cevre);
+                Console.WriteLine("Alanı = " + alan);
+                Console.ReadKey();
+
+            }
+            if (islem == 6)
+            {
+                int kenar, cevre;
+                Console.WriteLine("kenar değerini giriniz");
+                kenar = Convert.ToInt32(Console.ReadLine());
+                cevre = kenar * 3;
+                Console.WriteLine("Çeveresi = " + cevre);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ MatematikMenusu/MatematikMenusu/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("MatematikMenusu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MatematikMenusu")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("d2ce65c9-05a8-4492-b6c5-e36abe5e35a8")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
